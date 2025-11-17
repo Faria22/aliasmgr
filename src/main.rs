7834da0d96025598421d977c5191a17f384cde61
Faria22 <fariafelipe22@hotmail.com>
2025-11-17 18:31:21 -0500

    Added the config to main

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ mod config;
 
 use clap::Parser;
 use cli::Cli;
+use config::io::load_config;
 
 fn main() {
-    let _cli = Cli::parse();
+    let cli = Cli::parse();
+    let config = load_config(&cli).expect("Failed to load configuration");
+    println!("Configuration loaded: {:?}", config);
 }
