e36bffe51cc915aca2aba7a716334830c937142f
Faria22 <fariafelipe22@hotmail.com>
2025-11-17 22:13:48 -0500

    Added logger configuration logic

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,27 @@ use clap::Parser;
 use cli::Cli;
 use config::io::load_config;
 use core::add::add_alias;
+use log::LevelFilter;
+use log::debug;
 
 fn main() {
     let cli = Cli::parse();
+
+    let level = if cli.quiet {
+        LevelFilter::Error
+    } else if cli.verbose {
+        LevelFilter::Info
+    } else if cli.debug {
+        LevelFilter::Debug
+    } else {
+        LevelFilter::Warn
+    };
+
+    env_logger::Builder::new()
+        .filter_level(level)
+        .parse_default_env()
+        .init();
+
     let config = load_config(cli.config.as_ref()).expect("Failed to load configuration");
-    println!("Configuration loaded: {:?}", config);
+    debug!("Configuration loaded: {:?}", config);
 }
