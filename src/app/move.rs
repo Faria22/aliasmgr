9a7fbf7f2b0488bfd9786034f70a72c90a280643
Faria22 <fariafelipe22@hotmail.com>
2025-11-19 19:17:21 -0500

    Updated the move cli command, and created the handle_move function in app/

--- src/app/move.rs
+++ src/app/move.rs
@@ -10,6 +10,27 @@ use crate::cli::interaction::create_non_existent_group;
 
 use log::info;
 
-pub fn handle_move(config: &mut Config, command: MoveCommand) -> Result<Outcome, Failure> {
-    todo!()
+pub fn handle_move(config: &mut Config, cmd: MoveCommand) -> Result<Outcome, Failure> {
+    match move_alias(config, &cmd.name, &cmd.new_group) {
+        Ok(outcome) => Ok(outcome),
+        Err(e) => match e {
+            Failure::GroupDoesNotExist => {
+                let new_group = cmd
+                    .new_group
+                    .expect("new_group has to be `Some` for this error to arise");
+                if create_non_existent_group(&new_group) {
+                    info!("Created new group '{}'", new_group);
+                    add_group(config, &new_group, true)?;
+                    move_alias(config, &cmd.name, &Some(new_group))
+                } else {
+                    info!(
+                        "User aborted moving alias '{}' to non-existent group '{}'",
+                        &cmd.name, &new_group
+                    );
+                    Ok(Outcome::NoChanges)
+                }
+            }
+            _ => Err(e),
+        },
+    }
 }
--- src/cli/move.rs
+++ src/cli/move.rs
@@ -5,7 +5,7 @@ pub struct MoveCommand {
     /// Name of the alias to move
     #[arg()]
     pub name: String,
-    /// New group name
+    /// New group name. If left blank, the alias will leave the group
     #[arg()]
-    pub new_group: String,
+    pub new_group: Option<String>,
 }
