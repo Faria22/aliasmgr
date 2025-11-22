2b6857ccfc48c461a6c697aca7741d4ca2550657
Faria22 <fariafelipe22@hotmail.com>
2025-11-22 08:00:53 -0500

    Added function to get the config env var string

--- src/app/config_path.rs
+++ src/app/config_path.rs
@@ -4,8 +4,12 @@ use std::path::PathBuf;
 
 use anyhow::{Result, bail};
 
+pub fn config_env_var() -> &'static str {
+    "ALIASMGR_CONFIG_PATH"
+}
+
 pub fn determine_config_path() -> Result<Option<PathBuf>> {
-    if let Ok(path) = env::var("ALIASMGR_CONFIG_PATH") {
+    if let Ok(path) = env::var(config_env_var()) {
         let path = PathBuf::from(path);
         if path.exists() {
             return Ok(Some(path));
